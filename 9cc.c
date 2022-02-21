7742b1fd76d4d3d6b7fd07aca363815640720dec
inu <inu6411e@gmail.com>
2022-02-21 22:53:26 +0900

    単項+と単項-を追加

--- 9cc.c
+++ 9cc.c
@@ -172,6 +172,7 @@ Node *new_node_num(int val)
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr() {
@@ -187,13 +188,20 @@ Node *expr() {
 
 Node *mul()
 {
-  Node *node = primary();
+  Node *node = unary();
   for (;;)
   {
-    if (consume('*')) node = new_node(ND_MUL, node, primary());
-    else if (consume('/')) node = new_node(ND_DIV, node, primary());
+    if (consume('*')) node = new_node(ND_MUL, node, unary());
+    else if (consume('/')) node = new_node(ND_DIV, node, unary());
     else return node;
   }
+}
+
+Node *unary()
+{
+  if (consume('+')) return primary();
+  if (consume('-')) return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
 }
 
 Node *primary()
